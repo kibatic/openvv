11c2e01b9b1c971439b0a2917cabc94a4a2eeaa4
Philippe Le Van <philippe.levan@kibatic.com>
2022-11-20 06:52:08 +0100

    bootstrap

--- assets/app.js
+++ assets/app.js
@@ -5,8 +5,11 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+const $ = require('jquery');
+require('bootstrap');
+
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
 import './styles/photo-sphere-viewer.min.css';
 
 // start the Stimulus application
